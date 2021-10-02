abbbb9469ac473cab0d3de72e33ef6354b8498ce
KASSE LOKESH BABU <82827162+lokesh4666@users.noreply.github.com>
2021-10-02 20:54:06 +0530

    Create hospitals.py

---
+++ hospitals.py
@@ -0,0 +1,8 @@
+Anineni Hospitals,6514237523,Pragathi Nagar
+Medicover Hospitals,7643984310,Miyapur
+Arakali chain of Hospitals,9263401452,Hitech city
+Parameshwara Blood bank,7162534981,Ramoji Nagar
+Annane Blood bank,6152374891,Velmala
+Pasupathi Blood bank,9174919218,Somajiguda
+Rainbow Childrens Hospital,7623478121,Dilshuk Nagar
+Apollo chain,6273541322,Nampally
